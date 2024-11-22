409d09ce6ceab61dc1dc4bf494a1af4f216f8150
Benjamin Davis <davis.benjamin41902@gmail.com>
2024-11-22 03:57:44 -0500

    navbar

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,6 +1,14 @@
-<nav class="p-4">
+<nav class="flex flex-row items-center justify-between p-8">
 	<div>
 		<h2 class="text-4xl font-bold">Graphing</h2>
 		<span class="font-light italic text-neutral-400">better name in progress...</span>
+	</div>
+
+	<div class="flex flex-row items-center gap-4">
+		<a
+			href="https://github.com/bmdavis419/graphs"
+			target="_blank"
+			class="text-neutral-200 hover:underline">Repo</a
+		>
 	</div>
 </nav>
