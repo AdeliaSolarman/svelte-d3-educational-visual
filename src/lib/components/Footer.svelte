4019cdf21ab1ac772a88d2377cd8f0cd8d320959
Adelia Solarman <adeliasolarman88@gmail.com>
2025-05-07 18:56:12 -0500

    changed config svelte file for vercel compatibility

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,6 +1,6 @@
 <footer class="flex w-full items-center justify-center py-8">
 	<a
-		href="https://LinkedIn.com/in/adelia-solarman"
+		href="https://www.linkedin.com/in/adeliasolarman/"
 		target="_blank"
 		class="font-light text-neutral-400 hover:underline">by Adelia S.</a
 	>
